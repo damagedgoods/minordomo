e09cc40709fff59de079b7d2299a41bc9bf69144
Diego Cano <diegocano@MacBook-Pro-de-Diego.local>
2024-03-18 16:32:38 +0100

    First version of listening & saving messages

--- minor_app/cron.py
+++ minor_app/cron.py
@@ -1,13 +1,36 @@
+import sys, os, django, datetime
+from django.utils import timezone
+from django.db.models import Max
+
+sys.path.append('/Users/diegocano/workspace/minordomo/')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minordomo.settings')
+django.setup()
+
 import requests
+from minor_app.models import Message
 
 def my_cron_job():
-    url = "https://api.telegram.org/bot6819497556:AAGcgaZv6Fbyritjq6SyGLSgHWw-QseBfck/getUpdates"
+
+    # Mirar el último leído
+    last_id = Message.objects.aggregate(Max('update_id'))
+    offset = 0
+    if last_id["update_id__max"] is not None:
+        offset = int(last_id["update_id__max"]) + 1
+
+    # Construir la llamada con el offset adecuado
+    url = "https://api.telegram.org/bot6819497556:AAGcgaZv6Fbyritjq6SyGLSgHWw-QseBfck/getUpdates?offset="+str(offset)
     response = requests.get(url)
     response_json = response.json()
-    print(response_json)
     messages = response_json['result']
+
+    # Grabar los mensajes en la base de datos
     for m in messages:
-        print(m['message']['text'])
+        print(m['message']['text']+" - "+str(m['update_id']))
+        new_message =  Message(text=m['message']['text'], date=timezone.now(), update_id = str(m['update_id']))
+        new_message.save()
+
+    # Procesar cada mensaje
+
 
 my_cron_job()
 
--- minordomo/settings.py
+++ minordomo/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    "minor_app.apps.MinorAppConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
