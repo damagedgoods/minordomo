b69372d0b208b92825ece9b5fca69a7291cc0858
Diego Cano <diegocano@MacBook-Pro-de-Diego.local>
2024-03-19 06:31:17 +0100

    Cleaning logs

--- minor_app/cron.py
+++ minor_app/cron.py
@@ -11,8 +11,6 @@ from minor_app.models import Message
 
 def my_cron_job():
 
-    print("Iniciando cron job..")
-
     # Mirar el último leído
     last_id = Message.objects.aggregate(Max('update_id'))
     offset = 0
@@ -32,9 +30,6 @@ def my_cron_job():
         new_message.save()
 
     # Procesar cada mensaje
-        
-    print("Finalizando cron job")
-
 
 my_cron_job()
 
